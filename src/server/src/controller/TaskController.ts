29c0df713106a5beff24ea478f68c581d3d60086
gonzales <gonzalesitokawa@gmail.com>
2023-01-16 21:09:31 +0900

    add: wrote index show resource

--- src/server/src/controller/TaskController.ts
+++ src/server/src/controller/TaskController.ts
@@ -1,12 +1,15 @@
 import express from 'express';
+import Task from '../model/TaskSchema';
 
 class TaskController {
   static index(req: express.Request, res: express.Response) {
-
+    const tasks = Task.find();
+    res.status(200).json({'tasks': tasks});
   }
 
   static show(req: express.Request, res: express.Response) {
-
+    const task = Task.find({_id: req.params._id});
+    res.status(200).json({'task': task});
   }
 
   static store(req: express.Request, res: express.Response) {
